9aa5288e7f6c7c02e5d3c5cb7b442cde24d96c37
Pablo <viniciuspablo801@gmail.com>
2025-09-17 22:00:39 -0300

    feito o cadastrar e listar

--- src/main/java/br/edu/iftm/PPWIIJAVA/repository/AviaoForcaAereaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.iftm.PPWIIJAVA.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.iftm.PPWIIJAVA.modelo.Aviao;
-
-public interface AviaoForcaAereaRepository extends JpaRepository<Aviao, Long> {
-
-}
